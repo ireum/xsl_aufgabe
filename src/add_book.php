5fd93ac79c7960b559c8e3f0c8d3c8abba2cac7d
ireum <wijaya.sudarta@competec.ch>
2017-08-18 16:55:53 +0200

    add book php file & css

--- src/add_book.php
+++ src/add_book.php
@@ -6,8 +6,19 @@
     <link rel="stylesheet" href="lib.css">
 </head>
 <body>
-    <header>
-        <h1>Add Book</h1>
-    </header>
+<header>
+    <h1>Add Book</h1>
+</header>
+<div class="add_book_form_container">
+    <form class="add_book_form" action="" method="post">
+        <input type="text" name="author" placeholder="Author">
+        <input type="text" name="title" placeholder="Title">
+        <input type="text" name="genre" placeholder="Genre">
+        <input type="number" step="0.05" name="price" placeholder="Price">
+        <input type="text" name="releaseDate" placeholder="Release Date (YYYY-MM-DD)">
+        <textarea name="description" placeholder="Description" cols="30" rows="10"></textarea>
+        <input type="submit" name="submit" class="submit_button">
+    </form>
+</div>
 </body>
 </html>
