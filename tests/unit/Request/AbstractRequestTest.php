7dc5971dea31937fc1bfa92a937b4a05473af55a
Wijaya Sudarta <wijaya.sudarta@competec.ch>
2017-09-01 10:09:26 +0200

    Finished AbstractRequestTest

---
+++ tests/unit/Request/AbstractRequestTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace library\requests;
+
+use library\responder\Uri;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class AbstractRequestTest
+ * @package library\requests
+ * @covers library\requests\AbstractRequest
+ * @covers library\requests\PostRequest
+ * @covers library\requests\GetRequest
+ * @uses library\responder\Uri
+ */
+class AbstractRequestTest extends TestCase
+{
+    /** @var PostRequest|GetRequest */
+    private $abstractRequest;
+
+    public function setUp()
+    {
+        $inputVariables = ['foo' => 'bar', 'bar' => 'baz'];
+        $server = ['REQUEST_URI' => 'https://www.library.test.ch/path'];
+        $this->abstractRequest = new PostRequest($inputVariables, $server);
+    }
+
+    public function testHasReturnsFalseIfKeyIsNotSet()
+    {
+        $this->assertFalse($this->abstractRequest->has('test'));
+    }
+
+    public function testHasReturnstrueIfKeyIsSet()
+    {
+        $this->assertTrue($this->abstractRequest->has('foo'));
+    }
+
+    public function testGetReturnsValueOfKeyIfKeyIsSet()
+    {
+        $this->assertSame('bar', $this->abstractRequest->get('foo'));
+    }
+
+    public function testGetReturnsEmptyStringIfKeyIsNotSet()
+    {
+        $actual = $this->abstractRequest->get('test');
+        $this->assertEquals('', $actual);
+    }
+
+    public function testGetUriReturnsUriObject()
+    {
+        $actual = $this->abstractRequest->getUri();
+        $this->assertInstanceOf(Uri::class, $actual);
+    }
+
+
+}
